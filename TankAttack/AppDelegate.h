c0d56fdce345cb83300e72575ca80312cf045f1c
Ruslan Ardashev <ardashev120101@gmail.com>
2015-01-15 13:33:36 -0500

    Initial Commit

---
+++ TankAttack/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TankAttack
+//
+//  Created by Ruslan Ardashev on 1/15/15.
+//  Copyright (c) 2015 Ruslan Ardashev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TankAttack/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  TankAttack
+//
+
+//  Copyright (c) 2015 Ruslan Ardashev. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
