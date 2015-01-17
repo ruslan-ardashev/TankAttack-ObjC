a94b713d50b32c4bd5236d92d32d0b8876cc08c0
Ruslan Ardashev <ardashev120101@gmail.com>
2015-01-17 12:10:07 -0500

    imports bullet header

--- TankAttack/Model/World.h
+++ TankAttack/Model/World.h
@@ -6,10 +6,14 @@
 //  Copyright (c) 2015 Ruslan Ardashev. All rights reserved.
 //
 
+@class BulletSprite;
+
 #import <SpriteKit/SpriteKit.h>
 #import "GameViewController.h"
 #import "Player.h"
 #import "Overlay.h"
+#import "BulletSprite.h"
+
 
 @interface World : SKScene
 
