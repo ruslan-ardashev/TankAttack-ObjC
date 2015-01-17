b925ffa4116aebd50133e0fef07dd2b6b7414d73
Ruslan Ardashev <ardashev120101@gmail.com>
2015-01-17 12:48:01 -0500

    new stub for HealthBar

---
+++ TankAttack/Sprites/HealthBar.h
@@ -0,0 +1,13 @@
+//
+//  HealthBar.h
+//  TankAttack
+//
+//  Created by Ruslan Ardashev on 1/17/15.
+//  Copyright (c) 2015 Ruslan Ardashev. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface HealthBar : SKNode
+
+@end
---
+++ TankAttack/Sprites/HealthBar.m
@@ -0,0 +1,13 @@
+//
+//  HealthBar.m
+//  TankAttack
+//
+//  Created by Ruslan Ardashev on 1/17/15.
+//  Copyright (c) 2015 Ruslan Ardashev. All rights reserved.
+//
+
+#import "HealthBar.h"
+
+@implementation HealthBar
+
+@end
