17b9a29a8049c5d2b88c341336442f311e62f134
Ruslan Ardashev <ardashev120101@gmail.com>
2015-01-17 13:50:34 -0500

    images and boss stub

---
+++ TankAttack/Sprites/Boss.m
@@ -0,0 +1,13 @@
+//
+//  Boss.m
+//  TankAttack
+//
+//  Created by Ruslan Ardashev on 1/17/15.
+//  Copyright (c) 2015 Ruslan Ardashev. All rights reserved.
+//
+
+#import "Boss.h"
+
+@implementation Boss
+
+@end
