391baef75dfefd8df01a6c3ee75b97027ac87673
Ruslan Ardashev <ardashev120101@gmail.com>
2015-01-17 03:25:07 -0500

    bulletSprite stub

---
+++ TankAttack/Sprites/BulletSprite.h
@@ -0,0 +1,13 @@
+//
+//  BulletSprite.h
+//  TankAttack
+//
+//  Created by Ruslan Ardashev on 1/17/15.
+//  Copyright (c) 2015 Ruslan Ardashev. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface BulletSprite : SKSpriteNode
+
+@end
