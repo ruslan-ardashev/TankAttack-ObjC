174bbdaad061c8d640e9a2c98f97b87551c7cb8c
Ruslan Ardashev <ardashev120101@gmail.com>
2015-01-17 13:23:56 -0500

    sprite stub

---
+++ TankAttack/Sprites/Sprite.m
@@ -0,0 +1,13 @@
+//
+//  Sprite.m
+//  TankAttack
+//
+//  Created by Ruslan Ardashev on 1/17/15.
+//  Copyright (c) 2015 Ruslan Ardashev. All rights reserved.
+//
+
+#import "Sprite.h"
+
+@implementation Sprite
+
+@end
