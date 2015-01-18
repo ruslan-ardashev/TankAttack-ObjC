898e80a49c9f962e10a556233545fd653ed1c3a2
Ruslan Ardashev <ardashev120101@gmail.com>
2015-01-18 16:57:13 -0500

    kamikazee minion implementation

---
+++ TankAttack/Sprites/KamikazeeMinion.h
@@ -0,0 +1,24 @@
+//
+//  Minion.h
+//  TankAttack
+//
+//  Created by Ruslan Ardashev on 1/17/15.
+//  Copyright (c) 2015 Ruslan Ardashev. All rights reserved.
+//
+
+#import "Enemy.h"
+#import "GameViewController.h"
+#import "World.h"
+
+
+@interface Minion : Enemy
+
+- (id)initWithX:(double)x WithY:(double)y WithLeftXLimit:(double)leftXLimit WithRightXLimit:(double)rightXLimit WithIsGoingRight:(Boolean)isGoingRight IntoWorld:(World *)world;
+
+@end
+
+
+
+
+
+
---
+++ TankAttack/Sprites/KamikazeeMinion.m
@@ -0,0 +1,170 @@
+//
+//  Minion.m
+//  TankAttack
+//
+//  Created by Ruslan Ardashev on 1/17/15.
+//  Copyright (c) 2015 Ruslan Ardashev. All rights reserved.
+//
+
+#import "Minion.h"
+
+@implementation Minion {
+    
+    double _leftXLimit, _rightXLimit;
+    Boolean _isGoingRight;
+    
+}
+
+- (id)initWithX:(double)x WithY:(double)y WithLeftXLimit:(double)leftXLimit WithRightXLimit:(double)rightXLimit WithIsGoingRight:(Boolean)isGoingRight IntoWorld:(id)world {
+    
+    self = [super initWithImageNamed:MINION_IMAGE_NAME];
+    
+    if (self) {
+        
+        _rightXLimit = rightXLimit;
+        _leftXLimit = leftXLimit;
+        
+        [self setFollowsPlayer:false];
+        [self initHealthBar];
+        [self setPosition:CGPointMake(x, y)];
+        [self setScale:SMALL_TANK_SCALE_FACTOR];
+        
+        [world addChild:self];
+        return self;
+        
+    }
+    
+    else {
+        
+        return nil;
+    
+    }
+    
+}
+
+- (void)initHealthBar {
+    
+    HealthBar *hb;
+    
+    if ([GameViewController difficulty] == 1) {
+        
+        hb = [[HealthBar alloc] initWithHealth:50.0 WithParentWidth:self.size.width WithParentHeight:self.size.height WithParent:self];
+        
+    }
+    
+    else if ([GameViewController difficulty] == 2) {
+        
+        hb = [[HealthBar alloc] initWithHealth:100.0 WithParentWidth:self.size.width WithParentHeight:self.size.height WithParent:self];
+        
+    }
+    
+    else if ([GameViewController difficulty] == 3) {
+        
+        hb = [[HealthBar alloc] initWithHealth:200.0 WithParentWidth:self.size.width WithParentHeight:self.size.height WithParent:self];
+        
+    }
+    
+    [self setHealthBar:hb];
+    
+}
+
+- (void)updateEnemyXY {
+    
+    if (![self isAlive]) {
+        
+        return;
+        
+    }
+    
+    if (_isGoingRight) {
+        
+        CGFloat rightDestination = [self position].x + [self size].width/2 + MINION_SPEED;
+        
+        if (rightDestination >= _rightXLimit) {
+            
+            _isGoingRight = false;
+            [self goLeft];
+            
+        }
+        // Update x ++
+        
+        else {
+            
+            [self goRight];
+            
+        }
+        
+    }
+    
+    else {
+        
+        // Update x --
+        CGFloat leftDestination = [self position].x - [self size].width/2 - MINION_SPEED;
+        
+        if (leftDestination <= _leftXLimit) {
+            
+            _isGoingRight = true;
+            [self goRight];
+            
+        }
+        
+        else {
+            
+            [self goLeft];
+            
+        }
+        
+    }
+    
+}
+
+- (void)goRight {
+    
+    CGPoint newPosition = CGPointMake([self position].x + MINION_SPEED, [self position].y);
+    [self setPosition:newPosition];
+    
+}
+
+- (void)goLeft {
+    
+    CGPoint newPosition = CGPointMake([self position].x - MINION_SPEED, [self position].y);
+    [self setPosition:newPosition];
+    
+}
+
+- (Boolean)isFiring {
+    
+    if ([self isAlive]) {
+        
+        return ((arc4random() % 100)/100.0f < 0.13);
+        
+    }
+    
+    else {
+        
+        return false;
+        
+    }
+    
+}
+
+@end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
