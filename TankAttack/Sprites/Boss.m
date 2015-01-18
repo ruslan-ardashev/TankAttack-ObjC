42a9d6ff1df2af409e47c107a85ff17b6f956c1d
Ruslan Ardashev <ardashev120101@gmail.com>
2015-01-18 13:01:12 -0500

    implements boss

--- TankAttack/Sprites/Boss.m
+++ TankAttack/Sprites/Boss.m
@@ -12,13 +12,14 @@
 
 - (id)initWithX:(double)x WithY:(double)y IntoWorld:(World *)world {
     
-    self = [super initWithImageNamed:MINION_IMAGE_NAME];
+    self = [super initWithImageNamed:BOSS_IMAGE_NAME];
     
     if (self) {
         
+        
         [self initHealthBar];
         [self setPosition:CGPointMake(x, y)];
-        [self setScale:SMALL_TANK_SCALE_FACTOR];
+        [self setScale:BOSS_TANK_SCALE_FACTOR];
         
         [world addChild:self];
         return self;
@@ -45,19 +46,19 @@
     
     if ([GameViewController difficulty] == 1) {
         
-        hb = [[HealthBar alloc] initWithHealth:50.0 WithParentWidth:self.size.width WithParentHeight:self.size.height WithParent:self];
+        hb = [[HealthBar alloc] initWithHealth:300.0 WithParentWidth:self.size.width WithParentHeight:self.size.height WithParent:self];
         
     }
     
     else if ([GameViewController difficulty] == 2) {
         
-        hb = [[HealthBar alloc] initWithHealth:100.0 WithParentWidth:self.size.width WithParentHeight:self.size.height WithParent:self];
+        hb = [[HealthBar alloc] initWithHealth:600.0 WithParentWidth:self.size.width WithParentHeight:self.size.height WithParent:self];
         
     }
     
     else if ([GameViewController difficulty] == 3) {
         
-        hb = [[HealthBar alloc] initWithHealth:200.0 WithParentWidth:self.size.width WithParentHeight:self.size.height WithParent:self];
+        hb = [[HealthBar alloc] initWithHealth:800.0 WithParentWidth:self.size.width WithParentHeight:self.size.height WithParent:self];
         
     }
     
@@ -65,16 +66,74 @@
     
 }
 
-- (void)updateEnemyXY {
-    
+- (void)updateEnemyXYWithPlayerX:(CGFloat)playerX {
+
     if (![self isAlive]) {
         
         return;
         
     }
     
-    NSLog(@"implement boss xy");
+    // Eliminate jitter
+    if (ABS(playerX - [self position].x) < 1) {
+        
+        return;
+        
+    }
+    
+    else if (playerX > [self position].x) {
+        
+        [self goRight];
+        
+    }
+    
+    else {
+        
+        [self goLeft];
+        
+    }
+    
+}
+
+- (Boolean)isFiring {
+    
+    if ([self isAlive]) {
+        
+        return ((arc4random() % 100)/100.0f < 0.13);
+        
+    }
+    
+    else {
+        
+        return false;
+        
+    }
+    
+}
+
+- (void)goRight {
+    
+    CGPoint newPosition = CGPointMake([self position].x + BOSS_SPEED, [self position].y);
+    [self setPosition:newPosition];
+    
+}
+
+- (void)goLeft {
+    
+    CGPoint newPosition = CGPointMake([self position].x - BOSS_SPEED, [self position].y);
+    [self setPosition:newPosition];
     
 }
 
 @end
+
+
+
+
+
+
+
+
+
+
+
