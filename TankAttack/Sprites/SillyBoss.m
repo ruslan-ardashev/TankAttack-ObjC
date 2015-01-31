7b588f77f47c2861ef53b1f7f1f7663aafb2842b
Ruslan Ardashev <ardashev120101@gmail.com>
2015-01-31 14:52:07 -0500

    implements silllyBoss enemy movement

--- TankAttack/Sprites/SillyBoss.m
+++ TankAttack/Sprites/SillyBoss.m
@@ -6,8 +6,62 @@
 //  Copyright (c) 2015 Ruslan Ardashev. All rights reserved.
 //
 
+#import "World.h"
 #import "SillyBoss.h"
 
-@implementation SillyBoss
+@implementation SillyBoss {
+    
+    double _leftLimit, _rightLimit;
+    
+}
+
+- (id)initWithX:(double)x WithY:(double)y IntoWorld:(World *)world WithLeftLimit:(double)left WithRightLimit:(double)right {
+    
+    if (self = [super initWithX:x WithY:y IntoWorld:world]) {
+        
+        _leftLimit = left;
+        _rightLimit = right;
+        return self;
+        
+    }
+    
+    else {
+        
+        return nil;
+        
+    }
+    
+}
+
+- (void)updateEnemyXYWithPlayerX:(CGFloat)playerX {
+    
+    if (![self isAlive]) {
+        
+        return;
+        
+    }
+    
+    // Eliminate jitter
+    if (ABS(playerX - [self position].x) < 1) {
+        
+        return;
+        
+    }
+    
+    else if ((playerX > [self position].x) && ([self position].x < _rightLimit)) {
+        
+        [self goRight];
+        
+    }
+    
+    else if ((playerX < [self position].x)
+             &&
+             ([self position].x > _leftLimit)) {
+        
+        [self goLeft];
+        
+    }
+    
+}
 
 @end
