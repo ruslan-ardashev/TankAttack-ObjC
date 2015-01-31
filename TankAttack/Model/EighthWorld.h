fd1e74a865bb387c3f7c86b755b775cfcb82a584
Ruslan Ardashev <ardashev120101@gmail.com>
2015-01-31 15:07:46 -0500

    adds an eight level

---
+++ TankAttack/Model/EighthWorld.h
@@ -0,0 +1,13 @@
+//
+//  EighthWorld.h
+//  TankAttack
+//
+//  Created by Ruslan Ardashev on 1/31/15.
+//  Copyright (c) 2015 Ruslan Ardashev. All rights reserved.
+//
+
+#import "World.h"
+
+@interface EighthWorld : World
+
+@end
---
+++ TankAttack/Model/EighthWorld.m
@@ -0,0 +1,13 @@
+//
+//  EighthWorld.m
+//  TankAttack
+//
+//  Created by Ruslan Ardashev on 1/31/15.
+//  Copyright (c) 2015 Ruslan Ardashev. All rights reserved.
+//
+
+#import "EighthWorld.h"
+
+@implementation EighthWorld
+
+@end
