2276d91af4899a268ed8740e2b403ca9a998340b
Ruslan Ardashev <ardashev120101@gmail.com>
2015-01-31 14:50:58 -0500

    adds new kind of enemy

---
+++ TankAttack/Sprites/SillyBoss.h
@@ -0,0 +1,13 @@
+//
+//  SillyBoss.h
+//  TankAttack
+//
+//  Created by Ruslan Ardashev on 1/30/15.
+//  Copyright (c) 2015 Ruslan Ardashev. All rights reserved.
+//
+
+#import "Boss.h"
+
+@interface SillyBoss : Boss
+
+@end
---
+++ TankAttack/Sprites/SillyBoss.m
@@ -0,0 +1,13 @@
+//
+//  SillyBoss.m
+//  TankAttack
+//
+//  Created by Ruslan Ardashev on 1/30/15.
+//  Copyright (c) 2015 Ruslan Ardashev. All rights reserved.
+//
+
+#import "SillyBoss.h"
+
+@implementation SillyBoss
+
+@end
