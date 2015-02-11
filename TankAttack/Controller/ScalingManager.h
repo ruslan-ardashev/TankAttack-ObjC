e6628d5d8af028f170b07a0276897d0d5fe2ab70
Ruslan Ardashev <ardashev120101@gmail.com>
2015-02-11 12:03:34 -0500

    adds a Scaling Manager for scaling to older devices

---
+++ TankAttack/Controller/ScalingManager.h
@@ -0,0 +1,13 @@
+//
+//  ScalingManager.h
+//  TankAttack
+//
+//  Created by Ruslan Ardashev on 2/10/15.
+//  Copyright (c) 2015 Ruslan Ardashev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ScalingManager : NSObject
+
+@end
