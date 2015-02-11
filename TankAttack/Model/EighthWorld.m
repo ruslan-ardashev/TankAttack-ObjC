37a1e97bcd9ab6ef59ddba7024c6cf2075cc9255
Ruslan Ardashev <ardashev120101@gmail.com>
2015-02-11 12:05:22 -0500

    makes 8th world easier

--- TankAttack/Model/EighthWorld.m
+++ TankAttack/Model/EighthWorld.m
@@ -40,9 +40,7 @@
     CGFloat oneTenthWidth = self.size.width/10;
     CGFloat oneTenthHeight = self.size.height/10;
     
-    [[Minion alloc] initWithX:0.5*oneTenthWidth WithY:8*oneTenthHeight      WithLeftXLimit:0.5*oneTenthWidth WithRightXLimit:3.5*oneTenthWidth WithIsGoingRight:true IntoWorld:self];
-    [[Minion alloc] initWithX:2.5*oneTenthWidth WithY:8.5*oneTenthHeight    WithLeftXLimit:2.5*oneTenthWidth WithRightXLimit:5.5*oneTenthWidth WithIsGoingRight:true IntoWorld:self];
-    [[Minion alloc] initWithX:4.5*oneTenthWidth WithY:9*oneTenthHeight      WithLeftXLimit:4.5*oneTenthWidth WithRightXLimit:7.5*oneTenthWidth WithIsGoingRight:true IntoWorld:self];
+    [[Minion alloc] initWithX:0.5*oneTenthWidth WithY:8*oneTenthHeight      WithLeftXLimit:0.5*oneTenthWidth WithRightXLimit:5.5*oneTenthWidth WithIsGoingRight:true IntoWorld:self];
     [[Minion alloc] initWithX:6.5*oneTenthWidth WithY:9.5*oneTenthHeight    WithLeftXLimit:6.5*oneTenthWidth WithRightXLimit:9.5*oneTenthWidth WithIsGoingRight:true IntoWorld:self];
 
     
