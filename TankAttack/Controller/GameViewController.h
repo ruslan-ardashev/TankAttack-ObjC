4ddf3f9a6bb1472cebc008b7edb2fe0698bd9c66
Ruslan Ardashev <ardashev120101@gmail.com>
2015-03-27 21:18:27 -0400

    moves defines to Defines.h

--- TankAttack/Controller/GameViewController.h
+++ TankAttack/Controller/GameViewController.h
@@ -8,24 +8,10 @@
 
 #import <UIKit/UIKit.h>
 #import <SpriteKit/SpriteKit.h>
+
+#import "Defines.h"
 #import "ScalingManager.h"
 #import "UIColor_HexColor.h"
-
-#define DEFAULT_FONT @"DamascusBold"
-
-#define PLAYER_SPEED 0.025              // Compensates for a crazy Joystick velocity
-
-#define BULLET_SPEED 3.5
-#define DELAY_BETWEEN_BULLETS 0.100     // seconds
-#define BULLET_DAMAGE 10
-#define BULLET_HEIGHT_COMPENSATION 5
-
-#define PLAYER_IMAGE_NAME @"tank"
-#define MINION_IMAGE_NAME @"minion"
-#define KAMIKAZEE_MINION_IMAGE_NAME @"kamikazeeminion"
-#define BOSS_IMAGE_NAME @"boss"
-
-#define START_LEVEL 0                   // debugging
 
 
 @interface GameViewController : UIViewController
---
+++ TankAttack/resources/Defines.h
@@ -0,0 +1,28 @@
+//
+//  Defines.h
+//  TankAttack
+//
+//  Created by Ruslan Ardashev on 3/27/15.
+//  Copyright (c) 2015 Ruslan Ardashev. All rights reserved.
+//
+
+#ifndef TankAttack_Defines_h
+#define TankAttack_Defines_h
+
+#define DEFAULT_FONT @"DamascusBold"
+
+#define PLAYER_SPEED 0.025              // Compensates for a crazy Joystick velocity
+
+#define BULLET_SPEED 3.5
+#define DELAY_BETWEEN_BULLETS 0.100     // seconds
+#define BULLET_DAMAGE 10
+#define BULLET_HEIGHT_COMPENSATION 5
+
+#define PLAYER_IMAGE_NAME @"tank"
+#define MINION_IMAGE_NAME @"minion"
+#define KAMIKAZEE_MINION_IMAGE_NAME @"kamikazeeminion"
+#define BOSS_IMAGE_NAME @"boss"
+
+#define START_LEVEL 0                   // debugging
+
+#endif
